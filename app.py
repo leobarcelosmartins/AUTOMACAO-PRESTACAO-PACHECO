8282f56dbaba853520fe6581dac9d9749a5bbf38
leobarcelosmartins <backuptatico@gmail.com>
2026-02-12 10:38:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ if 'dados_sessao' not in st.session_state:
 # --- SIDEBAR (Corrigido para manter Título e Métricas) ---
 with st.sidebar:
     st.image("https://cdn-icons-png.flaticon.com/512/3208/3208726.png", width=100)
-    st.title("Painel de Controlo")
+    st.title("Painel de Controle")
     st.markdown("---")
     
     total_anexos = sum(len(v) for v in st.session_state.dados_sessao.values())
@@ -317,4 +317,5 @@ if st.button("🚀 FINALIZAR E GERAR RELATÓRIO", type="primary", width='stretch
 st.caption("Desenvolvido por Leonardo Barcelos Martins")
 
     
+
 
